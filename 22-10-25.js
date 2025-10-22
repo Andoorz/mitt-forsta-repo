68b67a9a07bcea9d2d72091fa5e6fadf06f7a9fc
Andoorz <andershansson91@hotmail.com>
2025-10-22 10:48:46 +0200

    variabler

---
+++ 22-10-25.js
@@ -0,0 +1,8 @@
+let firstName = "Anders";
+const homeTown = "Hässleholm";
+
+console.log(firstName, homeTown);
+
+homeTown = "Osby";
+console.log(homeTown);
+
---
+++ övning22-10-25/app.js
@@ -0,0 +1,8 @@
+let firstName = "Anders";
+const homeTown = "Osby";
+
+console.log(firstName, homeTown);
+
+homeTown = "Hässleholm";
+
+console.log(homeTown);
